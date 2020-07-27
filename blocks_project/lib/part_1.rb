9ce2d679c9486e9e572ebea19a5c338962529c3a
paulzay <paulotieno2@gmail.com>
2020-07-27 17:47:23 +0300

    work on part 1 blocks

--- blocks_project/lib/part_1.rb
+++ blocks_project/lib/part_1.rb
@@ -9,3 +9,20 @@ end
 def count_positive_subarrays(arr)
   arr.count{ |subarr| subarr.sum > 0}
 end
+
+def aba_translate(str)
+  vowels = 'aeiou'
+  new_word = ''
+  str.each_char do |char|
+    if vowels.include?(char)
+      new_word += char + "b" + char
+    else
+      new_word += char
+    end
+  end
+  new_word
+end
+
+def aba_array(arr)
+  arr.map {|word| aba_translate(word)}
+end
